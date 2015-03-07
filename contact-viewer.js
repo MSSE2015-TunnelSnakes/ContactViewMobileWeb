fa468ff64451362a3e615719fc56a72695f602af
naspinski <89bronco>
2015-03-07 16:22:23 -0600

    Initial upload

--- contact-viewer.js
+++ contact-viewer.js
@@ -1,6 +1,7 @@
 
 var _apiKey = 'tunnelsnakes';
 var _contacts = {};
+var _contact = {};
 
 $(document).on('pagebeforeshow', '#home-page', function () {
 
@@ -10,11 +11,13 @@ $(document).on('pagebeforeshow', '#home-page', function () {
             var contactList = $('#contact-list').html('');
             for(var i = 0; i < data.contacts.length; i++) {
                 var c = data.contacts[i];
-                _contacts[c._id] = contact
+                _contacts[c._id] = c;
                 $('<li>').append(
                     $('<a>')
-                        .append($('<h3>').html(c.name))
-                        .append($('<p>').html(c.email))
+                        .prop('href', '#details-page')
+                        .data('contact-id', c._id)
+                        .append($('<h3>').html(c.name + ', ' + c.title))
+                        //.append($('<p>').html(c.email))
                 ).appendTo(contactList);
             }
             contactList.listview('refresh');
@@ -23,3 +26,11 @@ $(document).on('pagebeforeshow', '#home-page', function () {
 
 });
 
+$(document).on('click', '#contact-list a', function() {
+    _contact = _contacts[$(this).data('contact-id')];
+    return true;
+});
+
+$(document).on('pagebeforeshow', '#details-page', function() {
+    $('#contact-details').html(_contact.name);
+});
